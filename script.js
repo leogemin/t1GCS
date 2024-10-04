a0e5994a12fa10fdf0c5c65aaee6a1404c373a81
GabrielHoppe <gabrielhoppecml@gmail.com>
2024-10-04 18:56:26 -0300

    Add row
    função de adicionar nova row.

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 const toggleTheme = () => {
       document.querySelector('body').classList.toggle('darkmode')
 }
+
+const addNew = () => {
+      const tabela = document.querySelector(".table")
+      const primeiraRow = tabela.children[1]
+
+      const primeiroInput = primeiraRow.querySelector("input")
+
+      if(primeiroInput.disabled){
+
+            const newRow = document.createElement("div")
+            newRow.className = "row"
+
+            for(let i = 0; i<7;i++){
+                  const divv = document.createElement("div")
+                  divv.className = "cell"
+
+                  const inputt = document.createElement("input")
+                  inputt.type = "text"
+                  inputt.value = ""
+
+                  divv.appendChild(inputt)
+
+                  newRow.appendChild(divv)
+            }
+
+            tabela.insertBefore(newRow, primeiraRow)
+      }
+}
